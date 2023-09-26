fa37a2ca7c20ab2b2e05d3a6e04e4f22f5feae67
Cody Marshall <codymstuntsx@gmail.com>
2023-09-26 14:57:37 -0700

    REFACTOR: setup for mobile and desktop

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const links = [
   {
@@ -21,21 +23,67 @@ const links = [
 ];
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
-    <nav className="flex justify-between items-center z-50 fixed top-[55px] inset-x-0 mx-auto left-0 w-11/12  bg-[#FFFFFF66] p-4 rounded-[90px] z-50">
-      <Image
-        src="/logo/logo_location.png"
-        alt="logo"
-        width={200}
-        height={100}
-      />
-      <menu className="flex gap-4">
-        {links.map((link) => (
-          <Link key={link.id} href={link.path} className="text-[#00000066]">
-            {link.name}
-          </Link>
-        ))}
-      </menu>
-    </nav>
+    <div className="relative">
+      <nav className="flex justify-between items-center z-50 fixed top-12 inset-x-0 mx-auto left-0 w-11/12 bg-[#FFFFFF66] p-4 rounded-[90px]">
+        <Image
+          src="/logo/logo_location.png"
+          alt="logo"
+          width={200}
+          height={100}
+        />
+
+        {/* Desktop Menu */}
+        <div className="hidden lg:flex space-x-6">
+          {links.map((link) => (
+            <Link key={link.id} href={link.path} className="text-[#00000066]">
+              {link.name}
+            </Link>
+          ))}
+        </div>
+
+        <button
+          className="lg:hidden text-[#00000066]"
+          onClick={toggleMenu}
+          aria-label="Toggle Menu"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            {menuOpen ? (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            ) : (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            )}
+          </svg>
+        </button>
+      </nav>
+
+      {/* Mobile Menu */}
+    </div>
   );
 }
