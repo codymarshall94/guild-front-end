8c1233d54c759f76f4b068e2f268d7beadc76867
Cody Marshall <codymstuntsx@gmail.com>
2023-09-26 14:57:58 -0700

    ADDED: involved section

--- scenes/home/Home.tsx
+++ scenes/home/Home.tsx
@@ -1,5 +1,6 @@
 import About from "./components/about/About";
 import Hero from "./components/hero/Hero";
+import Involved from "./components/involved/Involved";
 import Resources from "./components/resources/Resources";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <About />
       <Resources />
+      <Involved />
     </main>
   );
 }
