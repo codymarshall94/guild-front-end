6a074487abadead70aef867c4f75a72630565fc1
Cody Marshall <codymstuntsx@gmail.com>
2023-10-22 17:34:25 -0700

    Destructured join properties

--- scenes/home/components/hero/Hero.tsx
+++ scenes/home/components/hero/Hero.tsx
@@ -28,22 +28,23 @@ export default function Hero() {
 
       <div className="flex flex-col justify-center relative z-40 w-full">
         <h1 className="leading-20 text-4xl md:text-6xl bg-gradient-to-bl from-neutral-900 to-purple-700 bg-clip-text text-transparent">
-          <span className="font-bold">Connect</span> with people,<br></br>{" "}
-          <span className="font-bold">grow</span> your skills, <br></br> &amp;{" "}
-          <span className="font-bold">Improve</span> the web.
+          <span className="font-bold">Connect</span> with people,
+          <br></br>
+          <span className="font-bold">grow</span> your skills,
+          <br></br> &amp; <span className="font-bold">Improve</span> the web.
         </h1>
 
         <div className="flex flex-col justify-center items-center gap-4 pt-24">
           <h2 className="font-bold text-center text-2xl md:text-4xl">
             Join Us
           </h2>
-          <div className="flex flex-col md:flex-row gap-4 p-4">
-            {join.map((item) => (
+          <div className="flex flex-col lg:flex-row gap-4 p-4">
+            {join.map(({ id, name, virtual, person }) => (
               <MeetupCard
-                title={item.name}
-                virtual={item.virtual}
-                person={item.person}
-                key={item.id}
+                title={name}
+                virtual={virtual}
+                person={person}
+                key={id}
               />
             ))}
           </div>
